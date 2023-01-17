d7d68dae9e46d03386377a1dd006557cd065dd87
Julio Garcia Perez <julio@local>
2023-01-17 22:01:47 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const hbs = require("hbs");
+const app = express();
+
+/** Configure view engine */
+app.set("view engine", "hbs");
+app.set("views", `${__dirname}/views`);
+
+hbs.registerPartials(__dirname + "/views/partials");
+
+/** Configure static files */
+app.use(express.static("public"));
+
+const router = require("./config/routes.config");
+app.use(router);
+
+app.listen(3000, () => {
+  console.log("Ready!");
+});
---
+++ users.service.js
@@ -0,0 +1,52 @@
+// Don't need to understand this code, just use its API
+
+const users = [
+  { id: "1", name: "Julio" },
+  { id: "2", name: "Carlos" },
+];
+
+const User = {
+  find() {
+    return new Promise((resolve) => {
+      window.setTimeout(() => {
+        resolve(users);
+      }, Math.random() * 1000);
+    });
+  },
+  findById(id) {
+    return new Promise((resolve) => {
+      window.setTimeout(() => {
+        resolve(users.find((u) => u.id === id));
+      }, Math.random() * 1000);
+    });
+  },
+  create(user) {
+    return new Promise((resolve) => {
+      window.setTimeout(() => {
+        const newUser = { ...user, id: `${users.length + 1}` };
+        users.push(newUser);
+        resolve(newUser);
+      }, Math.random() * 1000);
+    });
+  },
+  findByIdAndUpdate(id, patch) {
+    return new Promise((resolve) => {
+      window.setTimeout(() => {
+        const user = users.find((u) => u.id === id);
+        Object.assign(user, patch);
+        resolve(user);
+      }, Math.random() * 1000);
+    });
+  },
+  findByIdAndDelete(id) {
+    return new Promise((resolve) => {
+      window.setTimeout(() => {
+        const user = users.find((u) => u.id === id);
+        users = users.filter((u) => u !== user);
+        resolve(user);
+      }, Math.random() * 1000);
+    });
+  },
+};
+
+module.exports = User;
